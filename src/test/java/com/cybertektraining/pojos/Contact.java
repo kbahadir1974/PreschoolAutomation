2dde40cd5902d61abaf1fbe196e2e3f3a3de6504
kardelen2011 <bahadirkamil20@gmail.com>
2020-04-02 03:24:03 -0400

    updates

---
+++ src/test/java/com/cybertektraining/pojos/Contact.java
@@ -0,0 +1,48 @@
+package com.cybertektraining.pojos;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Contact {
+
+    private   String emailAddress;
+    private  String phone;
+    @SerializedName("premanentAddress")
+    private  String permanentAddress;
+
+    public Contact(){
+
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPermanentAddress() {
+        return permanentAddress;
+    }
+
+    public void setPermanentAddress(String permanentAddress) {
+        this.permanentAddress = permanentAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "emailAddress='" + emailAddress + '\'' +
+                ", phone='" + phone + '\'' +
+                ", permanentAddress='" + permanentAddress + '\'' +
+                '}';
+    }
+}
